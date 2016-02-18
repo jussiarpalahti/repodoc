b5d8d33da9022899e12f755d6ef47b39916fddc2
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2016-02-18 11:04:40 +0200

    Function creates a map of all path : its filenames pairs
    Also found from SO a way to do list comparison
    more closely to how JS and Python does it:
    lexicographically, not lengthwise

--- src/cljs/repodoc/utils.cljs
+++ src/cljs/repodoc/utils.cljs
@@ -12,3 +12,28 @@
            (map (fn [x]
                   [(decorator x) x])
                 sortable))))
+
+(defn sapling
+  "Function that reduces into acc
+  a map of given items' basenames
+  using directory paths as keys"
+  [acc item]
+  (let [path (get item "path")
+        parts (clojure.string/split path #"/")
+        dirs (clojure.string/join "/" (butlast parts))
+        dir (if (empty? dirs) "/" dirs)
+        leaf (get acc dir)]
+    (if leaf
+      (assoc acc dir (conj leaf item))
+      (assoc acc dir [item]))))
+
+(defn path-reduce [paths]
+  (reduce sapling  {} paths))
+
+(defn vector-compare [[value1 & rest1] [value2 & rest2]]
+  "From http://stackoverflow.com/questions/15716894/"
+  (let [result (compare value1 value2)]
+    (cond
+      (not (= result 0)) result
+      (nil? value1) 0 ; value2 will be nil as well
+      :else (recur rest1 rest2))))
