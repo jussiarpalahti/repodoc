ed4578253e4aa70a828f2d9139aeccceab608044
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2016-02-20 20:13:13 +0200

    Added utility functions
    min-vec, cut-vec and sort-paths together manage
    sorting paths of different depth lexicographically
    (meaning /a/b/c/d goes before /b).
    
    Also, binary tree from Joy of Clojure,
    and some less than working attempts on
    graph building...

--- src/cljs/repodoc/utils.cljs
+++ src/cljs/repodoc/utils.cljs
@@ -37,3 +37,64 @@
       (not (= result 0)) result
       (nil? value1) 0 ; value2 will be nil as well
       :else (recur rest1 rest2))))
+
+(defn add-to-map
+  "Addind values into :children of keys path
+  trying to keep whatever other values
+  the target map has"
+  [data keys val]
+  (update-in data
+             keys
+             (fn [old]
+               (if (nil? old)
+                 {:children [val]}
+                 (if (:children old)
+                   (update old :children
+                           (fn [older & arg]
+                             (conj older val))
+                   (assoc old :children [val])))))))
+
+;; Binary tree from JoC 2nd edition listing 9.1
+(defrecord TreeNode [val l r])
+
+(defn xconj [t v]
+  (println t v)
+  (cond
+    (nil? t)       (TreeNode. v nil nil)
+    (< v (:val t)) (TreeNode. (:val t) (xconj (:l t) v) (:r t))
+    :else          (TreeNode. (:val t) (:l t) (xconj (:r t) v))))
+
+(defn xseq [t]
+  (when t
+    (concat (xseq (:l t)) [(:val t)] (xseq (:r t)))))
+
+(def sample-tree (reduce xconj nil [3 5 2 4 6]))
+(xseq sample-tree)
+
+(defrecord GitTree [name tree])
+
+(defn build-git-tree [t name tree]
+  "Build a tree node for given name
+  and add given list of names from the tree under it"
+  (cond
+    (nil? tree) (GitTree. name nil "blob")
+    :else (GitTree. name (map build-git-tree tree) "tree")))
+
+(defn min-vec
+  "Return largest vectors length"
+  [vecs]
+  (apply min (map count vecs)))
+
+(defn cut-vec
+  "Returns vectors cut to the size
+  of smallest vector"
+  [& vecs]
+  (let [size (min-vec vecs)]
+    (map #(subvec % 0 (min (count %) size)) vecs)))
+
+(defn sort-paths
+  "Sort paths lexicographically by comparing
+  path items directly and cutting vec length to
+  equal"
+  [paths]
+  (sort #(apply compare (cut-vec %1 %2)) paths))
